f2fca40e4abaf76edc507785548597ec62d3e4e3
Yuan Xue <yuanxue@stanford.edu>
2019-07-19 15:14:20 -0700

    cleaned up Snakemake_utils.py code, and added more documentations

--- Snakefile_utils.py
+++ Snakefile_utils.py
@@ -4,7 +4,9 @@ import numpy as np
 
 ##### Function for loading all R1 and R2 fastqs
 def get_all_fqgz(wildcards):
-    """ Return all samples containing read 1 and read 2 under current folder """
+    '''
+    Return all samples containing read 1 and read 2 under current folder
+    '''
     r1 = glob.glob('{sample}/*R1*.fastq.gz'.format(sample=wildcards.all_samples))
     r2 = glob.glob('{sample}/*R2*.fastq.gz'.format(sample=wildcards.all_samples))
     assert len(r1) == 1, '{sample}'.format(wildcards.all_samples)
@@ -13,22 +15,34 @@ def get_all_fqgz(wildcards):
 
 ##### Function for loading all bams
 def get_all_bams(wildcards):
-    """ Return all samples containing bams under current folder """
+    '''
+    Return all samples containing bams under current folder
+    '''
     bams = glob.glob('{sample}/*/*.bam'.format(sample=wildcards.all_samples))
     return [bams]
 
 
-# ##### Functions for getting file names and unzipping files
+##### Functions for getting file names and unzipping files
 def get_all_files(d):
+    '''
+    return all files in a directory
+    '''
     return [d+"/"+f for f in os.listdir(d) if os.path.isfile(os.path.join(d, f))]
 
 def unzip_fastq(f):
+    '''
+    function to gunzip .gz
+    '''
     cmd = "gunzip " + f
     p = subprocess.Popen(cmd, shell=True)
     return None
 
 ## Functions for merging tables
 def merge_htseq_tables(matches, outfile):
+    '''
+    merge a list of htseq tables defined by the list matches. Saves the merged
+    output as outfile.
+    '''
 # Print directory containing each file
     print(matches)
     print(outfile)
@@ -64,6 +78,10 @@ def merge_htseq_tables(matches, outfile):
             out.write(line + "\n")
 
 def merge_star_tables(matches, outfile):
+    '''
+    merge a list of star tables defined by the list matches. Saves the merged
+    output as outfile.
+    '''
 # Print directory containing each file
     print(matches)
     print(outfile)
@@ -140,19 +158,12 @@ def merge_star_tables(matches, outfile):
 
 ## Preprocessing of expression matrix
 
-''' Convert transcript expression matrix to gene expression matrix '''
-def convert_transcript_to_gene(infile, transcript_annotation, outfile,
-                       remove_last_nrows=5):
-    X = load_dataframes(infile, remove_last_nrows=remove_last_nrows)
-    ann = pd.read_csv(transcript_annotation, sep='\t', index_col=0)
-    Y = convert_id_to_gene(X.T, ann, start='transcript', end='gene').T
-    Y.to_csv(outfile, sep='\t', compression='gzip')
-
-''' Load expression matrix as pandas dataframe. Typically we remove the last
-    five rows from htseq output as they contain counting parameters.
-'''
 def load_dataframes(input_list, axis=1, remove_last_nrows=0,
                     strip_header=True, sample_prefix=[], transpose=False):
+    '''
+    Load expression matrix as pandas dataframe. Typically we remove the last
+    five rows from htseq output as they contain counting parameters.
+    '''
     df = pd.DataFrame()
     if type(input_list) is str:
         input_list = [input_list]
@@ -184,8 +195,21 @@ def load_dataframes(input_list, axis=1, remove_last_nrows=0,
                     else x for x in df.index.values]
     return df
 
-''' Convert the row names from transcript to gene '''
+def convert_transcript_to_gene(infile, transcript_annotation, outfile,
+                       remove_last_nrows=5):
+    '''
+    Convert transcript expression matrix to gene expression matrix
+    '''
+    X = load_dataframes(infile, remove_last_nrows=remove_last_nrows)
+    ann = pd.read_csv(transcript_annotation, sep='\t', index_col=0)
+    Y = convert_id_to_gene(X.T, ann, start='transcript', end='gene').T
+    Y.to_csv(outfile, sep='\t', compression='gzip')
+
+
 def convert_id_to_gene(X, gene_df, start='transcript', end='gene'):
+    '''
+    Convert feature_id expression matrix to transcript expression matrix
+    '''
     dictionary = {}
     for key, value in zip(gene_df[start].values, gene_df[end].values):
         if type(key) is str:
@@ -230,13 +254,19 @@ def load_chunks(infile, postfix=''):
     return load
 
 ## Scripts to make dataframe mapping exon/transcript to gene and products
-''' Figure out transcript annotation name '''
 def transcript_annotation_name(infile):
+    '''
+    Figure out transcript annotation name
+    '''
     filename = infile.split('/')[-1].split('.')[0]
     dirname = '/'.join(infile.split('/')[:-1])
     return os.path.join(dirname, filename)
 
 def make_annotation_dataframe(infile, outfile):
+    '''
+    Create a dataframe that stores exon/transript names with their
+    corresponding transcript/gene/product names.
+    '''
     if outfile.split('.')[-1] != 'gz':
         outfile += '.gz'
     outpath = os.path.abspath(outfile)
@@ -244,20 +274,27 @@ def make_annotation_dataframe(infile, outfile):
     if '.' in outfile.split('/')[-1]:
         outname_exon = outpath+'/'+outfile.split('/')[-1].split('.')[0] + \
                '_exon_annotation.tsv.gz'
-        outname_transcript = outpath+'/'+outfile.split('/')[-1].split('.')[0] + \
-               '_transcript_annotation.tsv.gz'
+        outname_transcript = outpath+'/'+outfile.split('/')[-1].split('.')[0]\
+                +'_transcript_annotation.tsv.gz'
     else:
         outname_exon = outpath+'/exon_annotation.tsv.gz'
         outname_transcript = outpath+'/transcript_annotation.tsv.gz'
     print('Saving to {:}'.format(outname_exon))
     print('Saving to {:}'.format(outname_transcript))
-    t_interest_type = ['transcript', 'mRNA', 'tRNA', 'ncRNA', 'rRNA', 'pseudogenic_transcript']
+    t_interest_type = ['transcript', 'mRNA', 'tRNA', 'ncRNA', 'rRNA',
+                       'pseudogenic_transcript']
     g_interest_type = ['gene', 'pseudogene', 'ncRNA_gene']
     special_transcripts = ['ERCC-', 'transgene:']
     property_keywords = ['description=', 'Parent=', 'ID=']
     null_word = 'Unknown'
 
     def check_if_transcript(X, feature_type):
+        '''
+        Check if the current feature is a transcript type. The feature is
+        identified as transcript if feature name contains "transcript:", or
+        "ENMUST" (mouse ensembl transcript prefix), or if the labeled feature
+        type is in our list of transcript feature types.
+        '''
         is_transcript = False
         if X.split(':')[0] == 'transcript':
             is_transcript = True
@@ -270,6 +307,12 @@ def make_annotation_dataframe(infile, outfile):
         return is_transcript
 
     def check_if_gene(X, feature_type):
+        '''
+        Check if the current feature is a special type. The feature is
+        identified as transcript if feature name contains "gene:", or
+        "ENMUSG" (mouse ensembl gene prefix), or if the labeled feature
+        type is in our list of gene feature types.
+        '''
         is_gene = False
         if X.split(':')[0] == 'gene':
             is_gene = True
@@ -282,6 +325,10 @@ def make_annotation_dataframe(infile, outfile):
         return is_gene
 
     def check_if_special(X):
+        '''
+        Check if the feature is a special type. If it is, then its feature,
+        transcript, gene, and product names are all the same.
+        '''
         is_special = False
         for u in special_transcripts:
             if u in X:
