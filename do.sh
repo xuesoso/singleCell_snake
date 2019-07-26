191c44e52eae2525f3c492e6d57294448f515484
xuesoso <xuesoso@gmail.com>
2019-07-25 18:57:48 -0700

    added option to run specific rule, and to run in local mode.

--- do.sh
+++ do.sh
@@ -1,11 +1,18 @@
-# Specify log file
-MY_HOME=/oak/stanford/groups/quake/yuanxue
-SCRIPTDIR=$MY_HOME/resources/sc_pipeline/snakemake_pipeline/singleCell_snake
+#!/bin/bash
+
+# Users: Please specify cluster partition if running on cluster 
+SCRIPTDIR="$( cd "$(dirname "$0")" ; pwd -P )"
 DATETIME=$(date "+%Y_%m_%d_%H_%M_%S")
 LOGFILE_OUT=$SCRIPTDIR/log/Snakefile.$DATETIME.$1.log.out
 LOGFILE_ERR=$SCRIPTDIR/log/Snakefile.$DATETIME.$1.log.err
+PARTITION=owners,normal,quake
+JOBNAME=snake_scRNA_seq
 
-sbatch --output=$LOGFILE_OUT --error=$LOGFILE_ERR $SCRIPTDIR/scripts/run_snake.sh $1 $2
+if [ "$2" = "local" ]; then
+    sh $SCRIPTDIR/scripts/run_snake.sh $1 $2 $3
+else
+    sbatch --job-name=$JOBNAME --partition=$PARTITION --output=$LOGFILE_OUT --error=$LOGFILE_ERR $SCRIPTDIR/scripts/run_snake.sh $1 $2 $3
+fi
 
 echo Log is
 echo $LOGFILE_OUT
--- scripts/run_snake.sh
+++ scripts/run_snake.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 #
-#SBATCH --job-name=snake_scRNA_seq
-#
 #SBATCH --time=48:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --partition=owners,normal,quake
 # send mail to this address
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=yuanxue@stanford.edu
@@ -17,28 +14,39 @@ SCRIPTDIR=$MY_HOME/resources/sc_pipeline/snakemake_pipeline/singleCell_snake
 SNAKEFILE=$SCRIPTDIR/snakefile
 SNP_RULE=bam_to_vcf
 CONFIGFILE=$SCRIPTDIR/config/$1
-NJOBS=200
+if [ "$2" = "local" ]; then
+    NJOBS=10
+else
+    NJOBS=200
+    # Load bcftools if on Stanford Sherlock, otherwise
+    # you need to make sure system has access to bcftools & samtools
+    module load biology
+    module load bcftools/1.8
+    module load samtools
+fi
+
+if [ -z "$3" ]; then
+    echo "Running rule all"
+    TARGETRULE=all
+else    
+    echo "Running rule $3"
+    TARGETRULE=$3
+fi
 WAIT=120
 
 # Specify parameters for snakemake
 RESTART=1
 
-# Load bcftools if on Stanford Sherlock, otherwise you need to make sure system has access to bcftools
-module load biology
-module load bcftools/1.8
-module load samtools
-#
-
 if [ "$2" = "unlock" ]; then
-    snakemake all -s $SNAKEFILE --configfile $CONFIGFILE --unlock
+    snakemake $TARGETRULE -s $SNAKEFILE --configfile $CONFIGFILE --unlock
 elif [ "$2" = 'dry' ]; then
-    snakemake all --snakefile $SNAKEFILE --configfile $CONFIGFILE --cluster "sbatch --ntasks=1 --job-name={params.name} --cpus-per-task={threads} --partition={params.partition}  --mem={params.mem} --time={params.time} -o log/{params.name}.%j.log" --keep-target-files -j $NJOBS -w $WAIT -k --rerun-incomplete -n --restart-times $RESTART --quiet
-elif [ "$2" = 'forceall' ]; then
-    snakemake all --snakefile $SNAKEFILE --configfile $CONFIGFILE --cluster "sbatch --ntasks=1 --job-name={params.name} --cpus-per-task={threads} --partition={params.partition}  --mem={params.mem} --time={params.time} -o log/{params.name}.%j.log" --keep-target-files -j $NJOBS -w $WAIT -k --rerun-incomplete --restart-times $RESTART -F
+    snakemake $TARGETRULE --snakefile $SNAKEFILE --configfile $CONFIGFILE --cluster "sbatch --ntasks=1 --job-name={params.name} --cpus-per-task={threads} --partition={params.partition}  --mem={params.mem} --time={params.time} -o log/{params.name}.%j.log" --keep-target-files -j $NJOBS -w $WAIT -k --rerun-incomplete -n --restart-times $RESTART --quiet
+elif [ "$2" = 'force$TARGETRULE' ]; then
+    snakemake $TARGETRULE --snakefile $SNAKEFILE --configfile $CONFIGFILE --cluster "sbatch --ntasks=1 --job-name={params.name} --cpus-per-task={threads} --partition={params.partition}  --mem={params.mem} --time={params.time} -o log/{params.name}.%j.log" --keep-target-files -j $NJOBS -w $WAIT -k --rerun-incomplete --restart-times $RESTART -F
 elif [ "$2" = 'forcerun' ]; then
-    snakemake all --snakefile $SNAKEFILE --configfile $CONFIGFILE --cluster "sbatch --ntasks=1 --job-name={params.name} --cpus-per-task={threads} --partition={params.partition}  --mem={params.mem} --time={params.time} -o log/{params.name}.%j.log" --keep-target-files -j $NJOBS -w $WAIT -k --rerun-incomplete --restart-times $RESTART -R
-elif [ "$2" = 'snp' ]; then
-    snakemake all --snakefile $SNP_RULE --configfile $CONFIGFILE --cluster "sbatch --ntasks=1 --job-name={params.name} --cpus-per-task={threads} --partition={params.partition}  --mem={params.mem} --time={params.time} -o log/{params.name}.%j.log" --keep-target-files -j $NJOBS -w $WAIT -k --rerun-incomplete --restart-times $RESTART -R
+    snakemake $TARGETRULE --snakefile $SNAKEFILE --configfile $CONFIGFILE --cluster "sbatch --ntasks=1 --job-name={params.name} --cpus-per-task={threads} --partition={params.partition}  --mem={params.mem} --time={params.time} -o log/{params.name}.%j.log" --keep-target-files -j $NJOBS -w $WAIT -k --rerun-incomplete --restart-times $RESTART -R
+elif [ "$2" = 'local' ]; then
+    snakemake $TARGETRULE --snakefile $SNAKEFILE --configfile $CONFIGFILE --keep-target-files -j $NJOBS -w $WAIT -k --rerun-incomplete --restart-times $RESTART
 else
-    snakemake all --snakefile $SNAKEFILE --configfile $CONFIGFILE --cluster "sbatch --ntasks=1 --job-name={params.name} --cpus-per-task={threads} --partition={params.partition}  --mem={params.mem} --time={params.time} -o log/{params.name}.%j.log" --keep-target-files -j $NJOBS -w $WAIT -k --rerun-incomplete --restart-times $RESTART
+    snakemake $TARGETRULE --snakefile $SNAKEFILE --configfile $CONFIGFILE --cluster "sbatch --ntasks=1 --job-name={params.name} --cpus-per-task={threads} --partition={params.partition}  --mem={params.mem} --time={params.time} -o log/{params.name}.%j.log" --keep-target-files -j $NJOBS -w $WAIT -k --rerun-incomplete --restart-times $RESTART
 fi
